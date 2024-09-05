5fe8da3989408e6f516b665d41a60bbd9f7a0f79
Kaya2005 <harveykaya1@gmail.com>
2024-09-06 00:25:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ global.antilink = false //DON'T CHANGE  / JANGAN GANTI
 global.versisc = '1.0.0' //DON'T CHANGE 
 global.sessionName = 'session'
 global.codeInvite = ""
-global.imageurl = 'https://telegra.ph/file/7c48252a7a35fd4d15339.jpg' //GANTI PP MU MENGGUNAKAN LINK TELEGRA PH
+global.imageurl = 'https://telegra.ph/file/8bc338cae5bbb5923aba7.jpg' //GANTI PP MU MENGGUNAKAN LINK TELEGRA PH
 global.isLink = 'https://whatsapp.com/channel/0029VaiPkRPLY6d0qEX50e2k' 
 global.thumb = fs.readFileSync("./thumb.png") ///DON'T CHANGE  
 global.audionya = fs.readFileSync("./all/sound.mp3") //DON'T CHANGE  
 global.packname = "KAYA" 
-global.author = "*KAYA SASAKI 👑*" 
+global.author = "*KAYA SASAKI*" 
 global.jumlah = "5" ////DON'T CHANGE
 
 let file = require.resolve(__filename)
