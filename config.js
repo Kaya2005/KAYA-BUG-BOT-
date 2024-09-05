06373e8faba430adcea7a0900cb8f145a6bd528b
Kaya2005 <harveykaya1@gmail.com>
2024-09-05 15:30:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 require("./all/module")
 
 global.owner = "https://whatsapp.com/channel/0029VaaUfPO8qIzztuf42D04" 
-global.namabot = "𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻𝟐" //BOT NAME
-global.namaCreator = "*PRECIOUS AYOMIDE*" //CREATOR NAME
+global.namabot = "KAYA BUG" //BOT NAME
+global.namaCreator = "*KAYA AYOMIDE*" //CREATOR NAME
 global.autoJoin = false //DON'T CHANGE  / JANGAN GANTI
 global.antilink = false //DON'T CHANGE  / JANGAN GANTI
 global.versisc = '1.0.0' //DON'T CHANGE 
 global.sessionName = 'session'
 global.codeInvite = ""
-global.imageurl = 'https://telegra.ph/file/6b841282edd26fa590b6c.jpg' //GANTI PP MU MENGGUNAKAN LINK TELEGRA PH
+global.imageurl = 'https://telegra.ph/file/6df60afff9a70b4986af2.jpg' //GANTI PP MU MENGGUNAKAN LINK TELEGRA PH
 global.isLink = 'https://whatsapp.com/channel/0029VaaUfPO8qIzztuf42D04' 
 global.thumb = fs.readFileSync("./thumb.png") ///DON'T CHANGE  
 global.audionya = fs.readFileSync("./all/sound.mp3") //DON'T CHANGE  
-global.packname = "𝐁𝐲 𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻𝟐 " 
-global.author = "*PRECIOUS AYOMIDE👑*" 
+global.packname = "KAYA" 
+global.author = "*KAYA AYOMIDE👑*" 
 global.jumlah = "5" ////DON'T CHANGE
 
 let file = require.resolve(__filename)
